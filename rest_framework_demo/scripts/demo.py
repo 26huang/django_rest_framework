c14725faae36c0b6532bb7661840341d7331816e
26huang <weishuoh@gmail.com>
2022-02-20 21:38:58 -0500

    serializer example

---
+++ rest_framework_demo/scripts/demo.py
@@ -0,0 +1,17 @@
+from api.models import Article
+from api.serializers import ArticleSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+# Insert data
+a = Article(title='Test 1', author='Wei', email='wei@demo.com')
+a.save()
+# serialize an object
+serializer = ArticleSerializer(a)
+print(serializer.data)
+# return object in JSON format
+content = JSONRenderer().render(serializer.data)
+print(content)
+# return all article data objects
+serializer = ArticleSerializer(Article.objects.all(), many=True)
+print(serializer.data)

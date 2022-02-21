1f85c9542d3a84245d61144ab5e273c2b15d4047
26huang <weishuoh@gmail.com>
2022-02-21 00:41:55 -0500

    Class based API to get, post, put, and delete data

--- rest_framework_demo/api/urls.py
+++ rest_framework_demo/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
 
 urlpatterns = [
     path('article/', article_list),
+    path('article2/', ArticleAPIView.as_view()),
     path('detail/<int:pk>', article_detail),
+    path('detail2/<int:id>', ArticleDetails.as_view()),
 ]
